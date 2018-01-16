3d5d10ccbd894b7ac7aaa3201040cd94e394fc0c
AwangDev <wangdeve@gmail.com>
2018-01-16 00:13:19 -0800

    update README.md

--- GenerateCodeTable.java
+++ GenerateCodeTable.java
@@ -106,14 +106,14 @@ public class GenerateCodeTable {
         //Assemble output
         String outputContent = "# Java Algorithm Problems\n\n" + 
             "### 程序员的一天\n" +
-            "从开始这个Github已经有将近两年时间, 很高兴这个repo可以帮到有需要的人. 我一直认为, 知识本身是无价的, 因此每逢闲暇, 我就会来维护这个repo, 给刷题的朋友们一些我的想法和见解. 下面来简单介绍一下这个repo:\n" + 
-            "**README.md**: 所有所做过的题目\n" + 
-            "**ReviewPage.md**: 所有题目的总结和归纳（不断完善中）\n" + 
-            "**KnowledgeHash2.md**: 对所做过的知识点的一些笔记\n" + 
-            "**SystemDesign.md**: 对系统设计的一些笔记\n" + 
-            "**Future Milestone**: 我准备将一些有意思的题目，做成视频的形式给大家参考\n" + 
-            "**借此机会, 正式介绍一下自己, 以及我背后的大老板**\n" + 
-            "[![介绍一下自己！](https://img.youtube.com/vi/3keMZsV1I1U/0.jpg)](https://youtu.be/3keMZsV1I1U)\n" + 
+            "从开始这个Github已经有将近两年时间, 很高兴这个repo可以帮到有需要的人. 我一直认为, 知识本身是无价的, 因此每逢闲暇, 我就会来维护这个repo, 给刷题的朋友们一些我的想法和见解. 下面来简单介绍一下这个repo:\n\n" + 
+            "**README.md**: 所有所做过的题目\n\n" + 
+            "**ReviewPage.md**: 所有题目的总结和归纳（不断完善中）\n\n" + 
+            "**KnowledgeHash2.md**: 对所做过的知识点的一些笔记\n\n" + 
+            "**SystemDesign.md**: 对系统设计的一些笔记\n\n" + 
+            "**Future Milestone**: 我准备将一些有意思的题目，做成视频的形式给大家参考\n\n" + 
+            "**借此机会, 正式介绍一下自己, 以及我背后的大老板**\n\n" + 
+            "[![介绍一下自己！](https://img.youtube.com/vi/3keMZsV1I1U/0.jpg)](https://youtu.be/3keMZsV1I1U)\n\n" + 
             "希望大家学习顺利, 对未来充满希望(程序员也是找到好老板的!)\n" + 
             "有问题可以给我写邮件(wangdeve@gmail.com), 或者在GitHub上发issue给我.\n\n" +         
             "| Squence | Problem       | Level  | Language  | Video Tutorial|\n" + 
--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 ### 程序员的一天
 从开始这个Github已经有将近两年时间, 很高兴这个repo可以帮到有需要的人. 我一直认为, 知识本身是无价的, 因此每逢闲暇, 我就会来维护这个repo, 给刷题的朋友们一些我的想法和见解. 下面来简单介绍一下这个repo:
+
 **README.md**: 所有所做过的题目
+
 **ReviewPage.md**: 所有题目的总结和归纳（不断完善中）
+
 **KnowledgeHash2.md**: 对所做过的知识点的一些笔记
+
 **SystemDesign.md**: 对系统设计的一些笔记
+
 **Future Milestone**: 我准备将一些有意思的题目，做成视频的形式给大家参考
+
 **借此机会, 正式介绍一下自己, 以及我背后的大老板**
+
 [![介绍一下自己！](https://img.youtube.com/vi/3keMZsV1I1U/0.jpg)](https://youtu.be/3keMZsV1I1U)
+
 希望大家学习顺利, 对未来充满希望(程序员也是找到好老板的!)
 有问题可以给我写邮件(wangdeve@gmail.com), 或者在GitHub上发issue给我.
 
